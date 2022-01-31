6eb660656821e6ed60fbc26156677bcbb67aa8ba
mertaksu28 <mertaksu28@outlook.com.tr>
2022-01-31 23:40:25 +0300

    added template and edit admin layot

---
+++ MVCBlog/Controllers/AdminCategoryController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace MVCBlog.Controllers
+{
+    public class AdminCategoryController : Controller
+    {
+        // GET: AminCategory
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
