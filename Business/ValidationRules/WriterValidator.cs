562f91b9999ab94966974c1a6b4baf6974bdcacc
mertaksu28 <mertaksu28@outlook.com.tr>
2022-02-04 20:57:13 +0300

    writers edited

--- Business/ValidationRules/WriterValidator.cs
+++ Business/ValidationRules/WriterValidator.cs
@@ -9,6 +9,7 @@ namespace Business.ValidationRules
         {
             RuleFor(c => c.WriterName).NotEmpty().WithMessage("Yazar Adı boş geçilemez");
             RuleFor(c => c.WriterSurName).NotEmpty().WithMessage("Yazar SoyAdı boş geçilemez");
+            RuleFor(c => c.WriterTitle).NotEmpty().WithMessage("Yazar Ünvanı boş geçilemez");
             RuleFor(c => c.WriterMail).NotEmpty().WithMessage("Mail alanı boş geçilemez");
             RuleFor(c => c.WriterName).MinimumLength(2).WithMessage("En az 2 karakter girişi yapınız");
             RuleFor(c => c.WriterSurName).MaximumLength(50).WithMessage("En fazla 50 karakter girişi yapılabilir");
--- MVCBlog/Controllers/WriterController.cs
+++ MVCBlog/Controllers/WriterController.cs
@@ -10,6 +10,7 @@ namespace MVCBlog.Controllers
     public class WriterController : Controller
     {
         WriterManager writerManager = new WriterManager(new EfWriterDal());
+        WriterValidator validationRules = new WriterValidator();
         public ActionResult Index()
         {
             var writerValues = writerManager.GetAll();
@@ -25,7 +26,6 @@ namespace MVCBlog.Controllers
         [HttpPost]
         public ActionResult AddWriter(Writer writer)
         {
-            WriterValidator validationRules = new WriterValidator();
             ValidationResult result = validationRules.Validate(writer);
             if (result.IsValid)
             {
@@ -41,5 +41,36 @@ namespace MVCBlog.Controllers
             }
             return View();
         }
+
+        [HttpGet]
+        public ActionResult UpdateWriter(int id)
+        {
+            var writerValue = writerManager.GetById(id);
+            return View(writerValue);
+        }
+
+        [HttpPost]
+        public ActionResult UpdateWriter(Writer writer)
+        {
+            ValidationResult result = validationRules.Validate(writer);
+            if (result.IsValid)
+            {
+                writerManager.Update(writer);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                foreach (var item in result.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
+        }
     }
 }
+
+/*
+ * 44.videoda ödev
+ * 45.videoda ödev
+ */
