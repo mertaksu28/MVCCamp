52899e4fbf71cacbda22d965fe7bf12179afe512
mertaksu28 <mertaksu28@outlook.com.tr>
2022-02-04 22:10:37 +0300

    adminLayout links operations and heading operations dropdown

---
+++ Business/Abstract/IHeadingService.cs
@@ -0,0 +1,14 @@
+﻿using Entities.Concrete;
+using System.Collections.Generic;
+
+namespace Business.Abstract
+{
+    public interface IHeadingService
+    {
+        List<Heading> GetAll();
+        Heading GetById(int id);
+        void Add(Heading heading);
+        void Delete(Heading heading);
+        void Update(Heading heading);
+    }
+}
---
+++ MVCBlog/Controllers/HeadingController.cs
@@ -0,0 +1,52 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace MVCBlog.Controllers
+{
+    public class HeadingController : Controller
+    {
+        HeadingManager headingManager = new HeadingManager(new EfHeadingDal());
+        CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+        WriterManager writerManager = new WriterManager(new EfWriterDal());
+        public ActionResult Index()
+        {
+            var headingValue = headingManager.GetAll();
+            return View(headingValue);
+        }
+
+        [HttpGet]
+        public ActionResult AddHeading()
+        {
+            List<SelectListItem> categoryValue = (from c in categoryManager.GetAll()
+                                                  select new SelectListItem
+                                                  {
+                                                      Text = c.CategoryName,
+                                                      Value = c.CategoryId.ToString()
+                                                  }).ToList();
+
+            List<SelectListItem> writerValue = (from w in writerManager.GetAll()
+                                                select new SelectListItem
+                                                {
+                                                    Text = w.WriterName + " " + w.WriterSurName,
+                                                    Value = w.WriterId.ToString()
+                                                }).ToList();
+
+            ViewBag.WriterValue = writerValue;
+            ViewBag.CategoryValue = categoryValue;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddHeading(Heading heading)
+        {
+            heading.HeadingDate = DateTime.Parse(DateTime.Now.ToLongTimeString());
+            headingManager.Add(heading);
+            return RedirectToAction("Index");
+        }
+    }
+}
