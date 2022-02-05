3a5c02be931fdb6849b14116dc8419ad6c757dd3
mertaksu28 <mertaksu28@outlook.com.tr>
2022-02-06 02:18:29 +0300

    message classes and partial view list menu

---
+++ Entities/Concrete/Message.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Entities.Concrete
+{
+    public class Message
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [StringLength(50)]
+        public string SenderMail { get; set; }
+
+        [StringLength(50)]
+        public string ReceiverMail { get; set; }
+
+        [StringLength(100)]
+        public string Subject { get; set; }
+
+        public string Content { get; set; }
+
+        public DateTime History { get; set; }
+    }
+}
--- MVCBlog/Controllers/ContactController.cs
+++ MVCBlog/Controllers/ContactController.cs
@@ -1,10 +1,6 @@
 ﻿using Business.Concrete;
 using Business.ValidationRules;
 using DataAccess.Concrete.EntityFramework;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace MVCBlog.Controllers
@@ -17,6 +13,17 @@ namespace MVCBlog.Controllers
         {
             var contactValues = contactManager.GetAll();
             return View(contactValues);
+        }
+
+        public ActionResult GetContactDetails(int id)
+        {
+            var contactValues = contactManager.GetById(id);
+            return View(contactValues);
+        }
+
+        public PartialViewResult MessageListMenu()
+        {
+            return PartialView();
         }
     }
 }
