233552e79cf72d1242c80c4d7cba11aabd4963e3
mertaksu28 <mertaksu28@outlook.com.tr>
2022-02-07 03:12:40 +0300

    error page

--- MVCBlog/Controllers/ErrorPageController.cs
+++ MVCBlog/Controllers/ErrorPageController.cs
@@ -1,16 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace MVCBlog.Controllers
 {
     public class ErrorPageController : Controller
     {
         // GET: ErrorPage
-        public ActionResult Index()
+        public ActionResult Page403()
         {
+            Response.StatusCode = 403;
+            Response.TrySkipIisCustomErrors = true;
+            return View();
+        }
+
+        public ActionResult Page404()
+        {
+            Response.StatusCode = 404;
+            Response.TrySkipIisCustomErrors = true;
             return View();
         }
     }
