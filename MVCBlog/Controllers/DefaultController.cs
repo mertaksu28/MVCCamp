f3a0add77ebad89c88172583ac1609dd0b15c442
mertaksu28 <mertaksu28@outlook.com.tr>
2022-02-15 22:47:24 +0300

    vitrin and getall headings and contents

---
+++ MVCBlog/Controllers/DefaultController.cs
@@ -0,0 +1,24 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using System.Web.Mvc;
+
+namespace MVCBlog.Controllers
+{
+    [AllowAnonymous]
+    public class DefaultController : Controller
+    {
+        HeadingManager headingManager = new HeadingManager(new EfHeadingDal());
+        ContentManager contentManager = new ContentManager(new EfContentDal());
+        public PartialViewResult Index()
+        {
+            var contentList = contentManager.GetAll();
+            return PartialView(contentList);
+        }
+
+        public ActionResult Headings()
+        {
+            var headingLists = headingManager.GetAll();
+            return View(headingLists);
+        }
+    }
+}
